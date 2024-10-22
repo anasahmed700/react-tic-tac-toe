bf250756afb80b94c9bdb45d06baa7478b7355fd
Anas Ahmed <anasahmed700@gmail.com>
2024-10-22 18:34:50 +0500

    working with state & component instance isolation

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
+
 const Player = ({ name, symbol }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    function handleEditClick() {
+        setIsEditing(true)
+    }
+
+    let playerName = <span className="player-name">{name}</span>;
+    if (isEditing) {
+        playerName = <input className="player-name" required />
+    }
     return (
         <li>
             <span className='player'>
-                <span className="player-name">{name}</span>
+                { playerName }
                 <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
         </li>
     )
 }
